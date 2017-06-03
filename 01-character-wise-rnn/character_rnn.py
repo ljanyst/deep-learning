b5d2b93baf07f5e4545ba3b89f46e92b5d289e4d
Lukasz Janyst <lukasz@jany.st>
2017-06-03 20:44:04 +0200

    character-wise-rnn: add name scopes

--- 01-character-wise-rnn/character_rnn.py
+++ 01-character-wise-rnn/character_rnn.py
@@ -22,34 +22,40 @@ class CharacterRNN:
         #-----------------------------------------------------------------------
         # Build the LSTM layers and the RNN
         #-----------------------------------------------------------------------
+        with tf.name_scope('inputs-encode'):
+            x_one_hot = tf.one_hot(self.inputs, n_classes)
+
         def lstm_cell():
             lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
             drop = tf.contrib.rnn.DropoutWrapper(
                      lstm, output_keep_prob = self.keep_prob)
             return drop
 
-        cell = tf.contrib.rnn.MultiRNNCell(
-                 [lstm_cell() for _ in range(lstm_layers)])
+        with tf.name_scope('rnn'):
+            cell = tf.contrib.rnn.MultiRNNCell(
+                     [lstm_cell() for _ in range(lstm_layers)])
 
-        self.initial_state = cell.zero_state(n_seqs, tf.float32)
+            self.initial_state = cell.zero_state(n_seqs, tf.float32)
 
-        x_one_hot = tf.one_hot(self.inputs, n_classes)
-
-        rnn_outputs, self.final_state = tf.nn.dynamic_rnn(cell, x_one_hot,
-                                          initial_state=self.initial_state)
+            rnn_outputs, self.final_state = tf.nn.dynamic_rnn(cell, x_one_hot,
+                                              initial_state=self.initial_state)
 
         #-----------------------------------------------------------------------
         # Build the fully connected layer
         #-----------------------------------------------------------------------
-        reshaped = tf.reshape(rnn_outputs, [-1, lstm_size])
-        with tf.variable_scope('logits'):
+        with tf.name_scope('rnn-reshape'):
+            reshaped = tf.reshape(rnn_outputs, [-1, lstm_size])
+
+        with tf.name_scope('fully-connected'):
             logits_w = tf.Variable(tf.truncated_normal((lstm_size, n_classes),
                                                        stddev=0.1))
             logits_b = tf.Variable(tf.zeros(n_classes))
+            self.logits = tf.matmul(reshaped, logits_w) + logits_b
+            tf.summary.histogram('logits_b', logits_b)
+            tf.summary.histogram('logits_w', logits_w)
 
-        self.logits = tf.matmul(reshaped, logits_w) + logits_b
-
-        self.outputs = tf.nn.softmax(self.logits, name='predictions')
+        with tf.name_scope('outputs'):
+            self.outputs = tf.nn.softmax(self.logits, name='predictions')
 
         #-----------------------------------------------------------------------
         # Set up some other members
@@ -61,19 +67,24 @@ class CharacterRNN:
         #-----------------------------------------------------------------------
         # Build a loss function
         #-----------------------------------------------------------------------
-        y_one_hot  = tf.one_hot(self.targets, self.n_classes)
-        y_reshaped = tf.reshape(y_one_hot, self.logits.get_shape())
+        with tf.name_scope('targets-encode'):
+            y_one_hot  = tf.one_hot(self.targets, self.n_classes)
+            y_reshaped = tf.reshape(y_one_hot, self.logits.get_shape())
 
-        loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits,
-                                                       labels=y_reshaped)
-        loss = tf.reduce_mean(loss)
+        with tf.name_scope('loss'):
+            loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits,
+                                                           labels=y_reshaped)
+            loss = tf.reduce_mean(loss)
+            tf.summary.scalar('loss', loss)
 
         #-----------------------------------------------------------------------
         # Build the optimizer
         #-----------------------------------------------------------------------
-        tvars     = tf.trainable_variables()
-        grads, _  = tf.clip_by_global_norm(tf.gradients(loss, tvars), grad_clip)
-        train_op  = tf.train.AdamOptimizer(learning_rate)
-        optimizer = train_op.apply_gradients(zip(grads, tvars))
+        with tf.name_scope('optimizer'):
+            tvars     = tf.trainable_variables()
+            grads, _  = tf.clip_by_global_norm(tf.gradients(loss, tvars),
+                                               grad_clip)
+            train_op  = tf.train.AdamOptimizer(learning_rate)
+            optimizer = train_op.apply_gradients(zip(grads, tvars))
 
         return loss, optimizer
